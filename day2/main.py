0b12e369c9074734f1d581005599ab8bf6f368d7
Roland Krancz <roland.krancz@gmail.com>
2025-12-02 18:40:37 +0100

    Day2-part1 refactor to use python naming conventions.

--- day2/main.py
+++ day2/main.py
@@ -1,33 +1,35 @@
-
-def Solution_part1(inputFile):
-    invalidIds = []
-    with open(inputFile, "r") as file:
+def solve_part1(input_file):
+    invalid_ids = []
+    with open(input_file, "r") as file:
         lines = file.readlines()
         for line in lines:
-            idRanges = line.split(",")
+            id_ranges = line.split(",")
 
-            for idRange in idRanges:
-                ids = idRange.split("-")
-                startId = int(ids[0])
-                endId = int(ids[1])
-                invalidIdsInRange = _getInvalidIdsInRange(startId, endId)
-                invalidIds.extend(invalidIdsInRange)
-    
-    return sum(invalidIds)
+            for id_range in id_ranges:
+                ids = id_range.split("-")
+                start_id = int(ids[0])
+                end_id = int(ids[1])
+                invalid_ids_in_range = _get_invalid_ids_in_range(start_id, end_id)
+                invalid_ids.extend(invalid_ids_in_range)
 
-def _getInvalidIdsInRange(startId, endId):
-    invalidIds = []
-    for id in range(startId, endId + 1):
-        idString = str(id)
-        strLength = len(idString)
-        idString_firstHalf = idString[:strLength // 2]
-        idString_secondHalf = idString[strLength // 2:]
-        
-        if idString_firstHalf == idString_secondHalf:
-            invalidIds.append(id)
-        
-    return invalidIds
+    return sum(invalid_ids)
+
+def _get_invalid_ids_in_range(start_id, end_id):
+    invalid_ids = []
+    for current_id in range(start_id, end_id + 1):
+        id_string = str(current_id)
+        midpoint = len(id_string) // 2
+        id_string_first_half = id_string[:midpoint]
+        id_string_second_half = id_string[midpoint:]
+
+        if id_string_first_half == id_string_second_half:
+            invalid_ids.append(current_id)
+
+    return invalid_ids
+
+def main():
+    result = solve_part1("input.txt")
+    print(f"Part 1 Result: {result}")
 
 if __name__ == "__main__":
-    result_part1 = Solution_part1("input.txt")
-    print(f"Part 1 Result: {result_part1}")
+    main()
