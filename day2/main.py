ed99b2db4c788179aa5ad4c58b71ceb2533a2f3a
Roland Krancz <roland.krancz@gmail.com>
2025-12-02 18:24:01 +0100

    Day2-part1 finished.

--- day2/main.py
+++ day2/main.py
@@ -1,8 +1,6 @@
 
-def GetInvalidIds(startId, endId):
-    return -1
-
 def Solution_part1(inputFile):
+    invalidIds = []
     with open(inputFile, "r") as file:
         lines = file.readlines()
         for line in lines:
@@ -12,11 +10,24 @@ def Solution_part1(inputFile):
                 ids = idRange.split("-")
                 startId = int(ids[0])
                 endId = int(ids[1])
-                invalidIds = GetInvalidIds(startId, endId)
-                print(f"Invalid IDs for range {startId}-{endId}: {invalidIds}")
-    return -1
+                invalidIdsInRange = _getInvalidIdsInRange(startId, endId)
+                invalidIds.extend(invalidIdsInRange)
+    
+    return sum(invalidIds)
 
+def _getInvalidIdsInRange(startId, endId):
+    invalidIds = []
+    for id in range(startId, endId + 1):
+        idString = str(id)
+        strLength = len(idString)
+        idString_firstHalf = idString[:strLength // 2]
+        idString_secondHalf = idString[strLength // 2:]
+        
+        if idString_firstHalf == idString_secondHalf:
+            invalidIds.append(id)
+        
+    return invalidIds
 
 if __name__ == "__main__":
     result_part1 = Solution_part1("input.txt")
-    print(f"Part 1 result: {result_part1}")
+    print(f"Part 1 Result: {result_part1}")
