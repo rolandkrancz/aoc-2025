6a5efb092e105204a639afbcf629f0431cf2ef24
Roland Krancz <roland.krancz@gmail.com>
2025-12-04 17:08:30 +0100

    Day4 - part2

--- day4/main.py
+++ day4/main.py
@@ -4,12 +4,39 @@ def solve_part1(input_file):
         grid = [list(line.strip()) for line in file]
 
     for x, row in enumerate(grid):
+        print(row)
         for y, cell in enumerate(row):
             if cell == "@":
                 adjacent_rolls = _count_adjacent_rolls(grid, x, y)
                 if adjacent_rolls < 4:
                     accessible_rolls += 1
+                    grid[x][y] = "x"
 
+    return accessible_rolls
+
+def solve_part2(input_file):
+    prev_accessible_rolls = -1
+    accessible_rolls = 0
+    
+    with open(input_file, "r", encoding="utf-8") as file:
+        grid = [list(line.strip()) for line in file]
+
+    while accessible_rolls > prev_accessible_rolls:
+        prev_accessible_rolls = accessible_rolls
+        accessible_rolls += _iterate(grid)
+    
+    return accessible_rolls
+
+def _iterate(grid):
+    accessible_rolls = 0
+    for x, row in enumerate(grid):
+        for y, cell in enumerate(row):
+            if cell == "@":
+                adjacent_rolls = _count_adjacent_rolls(grid, x, y)
+                if adjacent_rolls < 4:
+                    accessible_rolls += 1
+                    grid[x][y] = "x"
+    
     return accessible_rolls
 
 def _count_adjacent_rolls(grid, x, y):
@@ -28,7 +55,7 @@ def _count_adjacent_rolls(grid, x, y):
     return number_of_adjacent_rolls
 
 def main():
-    print(solve_part1("input.txt"))
+    print(solve_part2("input.txt"))
     return 0
 
 if __name__ == "__main__":
