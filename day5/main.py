62dce1cfb1767c07441d506a68da1b5723558356
Roland Krancz <roland.krancz@gmail.com>
2025-12-06 12:44:09 +0100

    Day6 - part1

--- day5/main.py
+++ day5/main.py
@@ -1,8 +1,11 @@
 
 def main():
     ingredients, ranges = get_input()
+    
     result = solve_part1(ingredients, ranges)
     print(f"Part 1 result: {result}")
+    result = solve_part2(ranges)
+    print(f"Part 2 result: {result}")
 
 def get_input():
     ranges = []
@@ -20,7 +23,7 @@ def get_input():
                 else:
                     range_parts = line.strip().split('-')
                     ranges.append((int(range_parts[0]), int(range_parts[1])))
-                    
+
     return ingredients, ranges
 
 def solve_part1(ingredients, ranges):
@@ -37,6 +40,27 @@ def _is_fresh(ingredient, ranges):
         if ingredient >= range[0] and ingredient <= range[1]:
             return True
     return False
+
+def solve_part2(ranges):
+    number_of_fresh_ids = 0
+    sorted_ranges = sorted(ranges, key=lambda x: x[0])
+
+    # Merge ranges
+    merged_ranges = [sorted_ranges[0]]
+    for current_range in sorted_ranges[1:]:
+        last_merged = merged_ranges[-1]
+
+        if current_range[0] <= last_merged[1]:
+            last_merged[1] = max(last_merged[1], current_range[1])
+        else:
+            merged_ranges.append(list(current_range))
+            
+    # Count
+    
+    for _range in merged_ranges:
+        number_of_fresh_ids += (_range[1] - _range[0] + 1)
+    
+    return number_of_fresh_ids
 
 if __name__ == "__main__":
     main()
