e9f0bbf35d5520565ce09a7f277527251cd5b28f
Roland Krancz <roland.krancz@gmail.com>
2025-12-10 08:44:51 +0100

    Day7 - part2

--- day7/main.py
+++ day7/main.py
@@ -21,10 +21,31 @@ def solve_part1(grid):
         tachyon_columns = new_tachyon_columns
     return number_of_splits
 
+def solve_part2(grid):
+    start_position = grid[0].find("S")
+    current_particles = {start_position: 1}
+
+    for current_row in range(1, len(grid)):
+        next_particles = {}
+        for col, count in current_particles.items():
+            if grid[current_row][col] == "^":
+                if col - 1 >= 0:
+                    next_particles[col - 1] = next_particles.get(col - 1, 0) + count
+                if col + 1 < len(grid[0]):
+                    next_particles[col + 1] = next_particles.get(col + 1, 0) + count
+            else:
+                next_particles[col] = next_particles.get(col, 0) + count
+
+        current_particles = next_particles
+
+    return sum(current_particles.values())
+
 def main():
     data = get_input("input.txt")
     result = solve_part1(data)
     print(f"Part 1 result: {result}")
+    result = solve_part2(data)
+    print(f"Part 2 result: {result}")
     
 if __name__ == "__main__":
     main()
